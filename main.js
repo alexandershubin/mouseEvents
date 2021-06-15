867a91baa1ac00cfcb7ff6a3a22281f9dfc25e6c
alexandershubin <shubin8@mail.ru>
2021-06-15 16:52:13 +0300

    master - refactor main.js

--- main.js
+++ main.js
@@ -3,19 +3,18 @@ const SQUARE_COUNT = 506;
 const colors = ['red', 'orange', 'blue', 'green', 'white', 'yellow'];
 
 const getRandomColor = () => {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
 
-const getColor = (element) => {
-  const color = getRandomColor()
-  element.style.background = color;
-  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+const getColor = ({target}) => {
+  const color = getRandomColor();
+  target.style.background = color;
+  target.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-const removeColor = (element) => {
-  element.style.background = '#da7777'
-  element.style.boxShadow = `0 0 2px #000`
+const removeColor = ({target}) => {
+  target.style.background = '#da7777'
+  target.style.boxShadow = `0 0 2px #000`
 }
 
 for (let i = 0; i < SQUARE_COUNT; i++) {
@@ -23,11 +22,6 @@ for (let i = 0; i < SQUARE_COUNT; i++) {
   square.classList.add('square');
   board.append(square);
 
-  square.addEventListener('mouseover', () => {
-    getColor(square)
-  })
-
-  square.addEventListener('mouseout', () => {
-    removeColor(square)
-  })
+  square.addEventListener('mouseover', getColor);
+  square.addEventListener('mouseout', removeColor);
 }
